860515ea2be310921671891b35f8891a927fb8e4
sonyanov <60970319+sonyanov@users.noreply.github.com>
2020-12-08 17:13:05 +0300

    lab8

---
+++ Lab 8/js/script.js
@@ -0,0 +1,9 @@
+function openid(id){
+	var openid = document.getElementById(id)
+	openid.style.display = "block"
+}
+
+function closeid(id){
+	var closeid = document.getElementById(id)
+	closeid.style.display = "none"
+}
