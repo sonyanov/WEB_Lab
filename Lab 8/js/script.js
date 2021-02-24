3d0c50e7810e8f50dd6aba26b8ad562936918a18
sonyanov <60970319+sonyanov@users.noreply.github.com>
2021-02-24 22:59:10 +0300

    Delete Lab 8 directory

--- Lab 8/js/script.js
+++
@@ -1,9 +0,0 @@
-function openid(id){
-	var openid = document.getElementById(id)
-	openid.style.display = "block"
-}
-
-function closeid(id){
-	var closeid = document.getElementById(id)
-	closeid.style.display = "none"
-}
